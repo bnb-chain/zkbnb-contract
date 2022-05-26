e761b51a1c196564aab4298b32e4bd09fa0f6a33
HinsLiu <hinsliubruyne@gmail.com>
2022-05-26 14:34:02 +0800

    [Hins] add new deployment scripts

--- scripts/deploy-local.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-npx hardhat --network local run .\scripts\zecrey-deploy.js > info\zecreyLegend.txt
---
+++ scripts/deploy/deploy-local.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+npx hardhat --network local run ./scripts/deploy.js > info/log.txt
