a141b18cf532e87d6ce6a040ee3a230731da3da1
Sri Krishna Mannem <sri.mannem@binance.com>
2022-11-17 12:22:59 +0100

    fix TS lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
