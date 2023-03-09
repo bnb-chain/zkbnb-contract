139ce3c1cb0f0a44ad793655e7b6f1a9d502c140
rain.z <rain.z@binance.com>
2023-03-09 14:38:49 +0300

    fix unit test

--- test/nft/AdditionalZkBNB.nft.test.ts
+++ test/nft/AdditionalZkBNB.nft.test.ts
@@ -1,27 +1,18 @@
 import { ethers } from 'hardhat';
-import { assert, expect } from 'chai';
 import { smock } from '@defi-wonderland/smock';
 
 describe('AdditionalZkBNB', function () {
-  let mockZNSController;
-  let mockNftFactory;
-
   let additionalZkBNB; // AdditionalZkBNBTest.sol
-  let owner, acc1;
+  let owner;
 
   before(async function () {
-    [owner, acc1] = await ethers.getSigners();
-
-    const MockZNSController = await smock.mock('ZNSController');
-    mockZNSController = await MockZNSController.deploy();
-    await mockZNSController.deployed();
+    [owner] = await ethers.getSigners();
 
     const MockGovernance = await smock.mock('Governance');
     const mockGovernance = await MockGovernance.deploy();
     await mockGovernance.deployed();
     await mockGovernance.setVariable('networkGovernor', owner.address);
 
-    mockNftFactory = await smock.fake('ZkBNBNFTFactory');
     const Utils = await ethers.getContractFactory('Utils');
     const utils = await Utils.deploy();
     await utils.deployed();
@@ -31,7 +22,7 @@ describe('AdditionalZkBNB', function () {
         Utils: utils.address,
       },
     });
-    additionalZkBNB = await AdditionalZkBNB.deploy(mockZNSController.address, mockGovernance.address);
+    additionalZkBNB = await AdditionalZkBNB.deploy(mockGovernance.address);
     await additionalZkBNB.deployed();
   });
 });
