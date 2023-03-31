118804d33f878478a6f2982c1ab2f61018720024
Joshua Q <99853910+jjqqbb@users.noreply.github.com>
2023-03-31 21:43:38 +0800

    remove creator logics and events from mintFromZkBNB

--- contracts/interfaces/INFTFactory.sol
+++ contracts/interfaces/INFTFactory.sol
@@ -3,18 +3,18 @@
 pragma solidity ^0.8.0;
 
 interface INFTFactory {
-  event MintNFTFromZkBNB(
-    address indexed _creatorAddress,
-    address indexed _toAddress,
-    uint256 _nftTokenId,
-    bytes _extraData
-  );
+  // event MintNFTFromZkBNB(
+  //   address indexed _creatorAddress,
+  //   address indexed _toAddress,
+  //   uint256 _nftTokenId,
+  //   bytes _extraData
+  // );
 
   function mintFromZkBNB(
-    address _creatorAddress,
+    // address _creatorAddress,
     address _toAddress,
     uint256 _nftTokenId,
-    string memory _nftTokenURI,
-    bytes memory _extraData
+    string memory _nftTokenURI
+    // bytes memory _extraData
   ) external;
 }
