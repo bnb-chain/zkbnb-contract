1f7e0199c0e52ed0302da986326287e93ac0ca7a
JQ <josh.q.bn@gmail.com>
2023-05-07 23:39:44 +0800

    feat: change optimizer runs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -28,7 +28,7 @@ const config: HardhatUserConfig = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
+        runs: 1500,
       },
       outputSelection: {
         '*': {
