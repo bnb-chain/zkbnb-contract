a37cbf27347216761fcef58ff6e875059f6ebd75
Razorback <raz.w@binance.com>
2023-05-08 10:56:51 +0400

    fix: fixed issues in lib/ according to audit report

--- contracts/lib/Bytes.sol
+++ contracts/lib/Bytes.sol
@@ -158,24 +158,60 @@ library Bytes {
   // Returns the newly created 'bytes memory'
   // NOTE: theoretically possible overflow of (_start + _length)
   function slice(bytes memory _bytes, uint256 _start, uint256 _length) internal pure returns (bytes memory) {
-    // bytes length should be larger than start byte + length bytes
-    require(_bytes.length >= (_start + _length), "Z");
+    require(_length + 31 >= _length, "slice_overflow");
+    require(_bytes.length >= _start + _length, "slice_outOfBounds");
 
-    bytes memory tempBytes = new bytes(_length);
+    bytes memory tempBytes;
 
-    if (_length != 0) {
-      assembly {
-        let slice_curr := add(tempBytes, 0x20)
-        let slice_end := add(slice_curr, _length)
+    assembly {
+      switch iszero(_length)
+      case 0 {
+        // Get a location of some free memory and store it in tempBytes as
+        // Solidity does for memory variables.
+        tempBytes := mload(0x40)
+
+        // The first word of the slice result is potentially a partial
+        // word read from the original array. To read it, we calculate
+        // the length of that partial word and start copying that many
+        // bytes into the array. The first word we copy will start with
+        // data we don't care about, but the last `lengthmod` bytes will
+        // land at the beginning of the contents of the new array. When
+        // we're done copying, we overwrite the full first word with
+        // the actual length of the slice.
+        let lengthmod := and(_length, 31)
+
+        // The multiplication in the next line is necessary
+        // because when slicing multiples of 32 bytes (lengthmod == 0)
+        // the following copy loop was copying the origin's length
+        // and then ending prematurely not copying everything it should.
+        let mc := add(add(tempBytes, lengthmod), mul(0x20, iszero(lengthmod)))
+        let end := add(mc, _length)
 
         for {
-          let array_current := add(_bytes, add(_start, 0x20))
-        } lt(slice_curr, slice_end) {
-          slice_curr := add(slice_curr, 0x20)
-          array_current := add(array_current, 0x20)
+          // The multiplication in the next line has the same exact purpose
+          // as the one above.
+          let cc := add(add(add(_bytes, lengthmod), mul(0x20, iszero(lengthmod))), _start)
+        } lt(mc, end) {
+          mc := add(mc, 0x20)
+          cc := add(cc, 0x20)
         } {
-          mstore(slice_curr, mload(array_current))
+          mstore(mc, mload(cc))
         }
+
+        mstore(tempBytes, _length)
+
+        //update free-memory pointer
+        //allocating the array padded to 32 bytes like the compiler does now
+        mstore(0x40, and(add(mc, 31), not(31)))
+      }
+      //if we want a zero-length slice let's just return a zero-length array
+      default {
+        tempBytes := mload(0x40)
+        //zero out the 32 bytes slice we are about to return
+        //we need to do it because Solidity does not garbage collect
+        mstore(tempBytes, 0)
+
+        mstore(0x40, add(tempBytes, 0x20))
       }
     }
 
